38495f4c1c8eaefc3be78b045d7e9abe22ca3bf6
Kelsey Florek <kelseyflorek22@gmail.com>
2026-01-21 11:35:01 -0600

    added dir creation

--- copy-vendor-assets.sh
+++ copy-vendor-assets.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+mkdir -p public/vendor/reveal
 cp -r node_modules/reveal.js/dist public/vendor/reveal
+mkdir -p public/vendor/reveal-plugins
 cp -r node_modules/reveal.js/plugin public/vendor/reveal-plugins
 cp -r node_modules/reveal.js-plugins public/vendor/reveal-plugins
+mkdir -p public/vendor/jquery
 cp -r node_modules/jquery/dist public/vendor/jquery
