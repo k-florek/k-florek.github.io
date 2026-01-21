036bd4e695115add85cd1817c72aed1f2f351b75
Kelsey Florek <kelseyflorek22@gmail.com>
2026-01-21 11:24:25 -0600

    fixed build time race condition

---
+++ copy-vendor-assets.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -r node_modules/reveal.js/dist public/vendor/reveal
+cp -r node_modules/reveal.js/plugin public/vendor/reveal-plugins
+cp -r node_modules/reveal.js-plugins public/vendor/reveal-plugins
+cp -r node_modules/jquery/dist public/vendor/jquery
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,25 +13,5 @@ export default defineNuxtConfig({
   plugins: [
     '~/plugins/fontawesome.js'
   ],
-  modules: ['@nuxt/content'],
-  hooks: {
-    async 'nitro:build:before'() {
-      // copy reveal into public
-      let src = path.resolve('node_modules/reveal.js/dist')
-      let dest = path.resolve('public/vendor/reveal')
-      await fs.copy(src, dest)
-      // copy reveal default plugins
-      src = path.resolve('node_modules/reveal.js/plugin')
-      dest = path.resolve('public/vendor/reveal-plugins')
-      await fs.copy(src, dest)
-      // copy reveal extra plugins
-      src = path.resolve('node_modules/reveal.js-plugins')
-      dest = path.resolve('public/vendor/reveal-plugins')
-      await fs.copy(src, dest)
-      // copy jquery plugins
-      src = path.resolve('node_modules/jquery/dist')
-      dest = path.resolve('public/vendor/jquery')
-      await fs.copy(src, dest)
-    }
-  }
+  modules: ['@nuxt/content']
 })
